cd4938679064a5bc78efc0a5eb37731bf1754b7b
Abhirajkar Bajpai <abhirajkar.bajpai@vyld.io>
2024-08-16 15:24:34 +0530

    emoji keyboard building

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,9 +1,11 @@
-import React, { useContext} from 'react';
+import React, { useContext } from 'react';
 import './Keyboard.css';
 import { SK_Context } from '../context/context';
+import { FaBackspace } from 'react-icons/fa'; // Import icons
 
 const EmojiKeyboard = ({ onEmojiClick, onSpaceClick, onDeleteClick, refr }) => {
-    const {emoji}=useContext(SK_Context);
+    const { emoji } = useContext(SK_Context);
+
     return (
         <div className="emoji-keyboard" ref={refr}>
             <div className="emoji-list">
@@ -12,10 +14,15 @@ const EmojiKeyboard = ({ onEmojiClick, onSpaceClick, onDeleteClick, refr }) => {
                         {emoj}
                     </button>
                 ))}
-            </div>
-            <div className="keyboard-controls">
-                <button onClick={onSpaceClick} className="control-button">Space</button>
-                <button onClick={onDeleteClick} className="control-button">Delete</button>
+                <div className='special-btn_EK'>
+                <button onClick={onSpaceClick} className="control-button space_EK">
+                    {/* <FaSpaceShuttle className="control-icon" /> */}
+                    space
+                </button>
+                <button onClick={onDeleteClick} className="control-button delete_EK">
+                    <FaBackspace  style={{ color: 'white' }} className="control-icon" />
+                </button>
+                </div>
             </div>
         </div>
     );
